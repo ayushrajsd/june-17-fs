5d5ee1bba1dd1b2e29a9b016bfd9885f98ed531d
Ayush <ayush.raj.sd@gmail.com>
2026-07-31 22:20:24 +0530

    kanban-2 problem 2

--- LLD2/DOM/kanban/script.js
+++ LLD2/DOM/kanban/script.js
@@ -9,6 +9,84 @@ let selectedColor = "ff6b6b";
 
 // === Modal toggling ===
 let isModalOpen = false;
+let ticketCounter = 0;
+
+// === HELPERS ===
+function generateId() {
+  ticketCounter++;
+  console.log(crypto.randomUUID());
+  return `ticket-${ticketCounter}`;
+}
+
+function createTicket(taskText, color) {
+  const id = generateId();
+
+  // create the ticket container div
+  const ticket = document.createElement("div");
+  ticket.classList.add("ticket");
+  ticket.setAttribute("data-id", id);
+  //   ticket.dataset.id = id;
+
+  // 2. create the ticker color band
+  const colorband = document.createElement("div");
+  colorband.classList.add("ticket-color-band");
+  colorband.style.backgroundColor = color;
+
+  // 3. text area
+  const text = document.createElement("div");
+  text.classList.add("ticket-text");
+  text.textContent = taskText;
+
+  // 4. locl icon
+  const lock = document.createElement("div");
+  lock.classList.add("ticket-lock");
+  lock.innerHTML = `<i class="fa-solid fa-lock-open"></i>`;
+
+  // assemble
+  ticket.append(colorband);
+  ticket.append(text);
+  ticket.append(lock);
+
+  // add the ticket to the poage
+  ticketContainer.appendChild(ticket);
+  return id;
+}
+
+function createTicket2(taskText, color) {
+  const ticketId = generateId();
+  const ticketElement = document.createElement("div");
+  ticketElement.classList.add("ticket");
+  ticketElement.setAttribute("data-id", ticketId);
+  ticketElement.innerHTML = `<div class="ticket-color-band" style="background-color: ${color};"></div> <div class="ticket-text"></div> <div class="ticket-lock"> <i class="fa-solid fa-lock-open"></i> </div>`;
+  const ticketTaskElement = ticketElement.querySelector(".ticket-text");
+  ticketTaskElement.textContent = taskText;
+  ticketContainer.appendChild(ticketElement);
+}
+
+/***
+ * 
+ * const createTicket = function(taskText, color) {
+const ticketId = generateUniqueId();
+const ticketElement = document.createElement("div");
+ticketElement.classList.add("ticket");
+ticketElement.setAttribute("data-id", ticketId);
+ticketElement.innerHTML = <div class="ticket-color-band" style="background-color: ${color};"></div> <div class="ticket-text"></div> <div class="ticket-lock"> <i class="fa-solid fa-lock-open"></i> </div>;
+const ticketTaskElement = ticketElement.querySelector(".ticket-text");
+ticketTaskElement.textContent = taskText;
+listContainer.appendChild(ticketElement);
+};
+ */
+
+/**
+ * 
+ *   <div class="ticket">
+        <div class="ticket-color-band" style="background-color: #ff6b6b"></div>
+        <div class="ticket-text">Buy groceries</div>
+        <div class="ticket-lock">
+          <i class="fa-solid fa-lock-open"></i>
+        </div>
+      </div>
+ */
 
 addBtn.addEventListener("click", function () {
   isModalOpen = !isModalOpen;
